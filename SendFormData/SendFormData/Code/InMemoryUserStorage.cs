5e34cdcdb9d044f744e16ba3d6d8257fadc05518
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-10-28 11:36:18 +0200

    Edit

---
+++ SendFormData/SendFormData/Code/InMemoryUserStorage.cs
@@ -0,0 +1,23 @@
+﻿using SendFormData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendFormData.Code
+{
+    public class InMemoryUserStorage
+    {
+        public static List<User> Users { get; set; }
+
+        static InMemoryUserStorage()
+        {
+            Users = new List<User>()
+            {
+                new User(){ ID = 1, UserName = "muhammed", Password = "123456789" },
+                new User(){ ID = 2, UserName = "alshaimaa", Password = "123456789" },
+                new User(){ ID = 3, UserName = "rofida", Password = "123456789" }
+            };
+        }
+    }
+}
---
+++ SendFormData/SendFormData/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using SendFormData.Code;
+using SendFormData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SendFormData.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult index()
+        {
+            return View(InMemoryUserStorage.Users);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User userLogin =
+                InMemoryUserStorage.Users.FirstOrDefault(s => s.UserName == user.UserName
+                && s.Password == user.Password);
+
+            if (userLogin == null)
+                return View();
+
+            //TempData["SelectedUser"] = userLogin;
+            //TempData
+            return Redirect("/User/Login");
+        }
+    }
+}
