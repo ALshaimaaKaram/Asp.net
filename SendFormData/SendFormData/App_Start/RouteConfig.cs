9cdeb20640863e1ef8ff758704abd416cd843510
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-10-30 01:19:49 +0200

    Constraint For Route

--- SendFormData/SendFormData/App_Start/RouteConfig.cs
+++ SendFormData/SendFormData/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SendFormData.Constraints;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,7 +18,11 @@ namespace SendFormData
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                //constraints: new
+                //{
+                //    CustomCostraint = new OnlyEdgeConstrint()
+                //}
+            ); 
         }
     }
 }
---
+++ SendFormData/SendFormData/Constraints/OnlyEdgeConstrint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SendFormData.Constraints
+{
+    public class OnlyEdgeConstrint : IRouteConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return httpContext.Request.UserAgent.Contains("MicrosoftEdge");
+        }
+    }
+}
